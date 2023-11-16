492b220dfee3986f96b65428fd4e3cd7bb626e99
NawidT <nawidtahmid@gmail.com>
2023-11-15 20:10:52 -0500

    updated webby name

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ origins = [
     "http://127.0.0.1:5173",
     "https://resume-nawidt.vercel.app",
     "https://resume-sigma-eosin.vercel.app",
-    "https://resume-sigma-eosin.vercel.app/"
+    "https://resume-sigma-eosin.vercel.app/",
+    "https://nawidtahmid.vercel.app/"
 ]
 app.add_middleware(
     CORSMiddleware,
